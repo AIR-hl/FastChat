d93ce03fd20d81cf9c8ac1f1a4945d4fa8c04bd4
Shiyue Xu <66808901+AIR-hl@users.noreply.github.com>
2025-01-16 16:11:13 +0800

    Update run.sh

--- fastchat/llm_judge/run.sh
+++ fastchat/llm_judge/run.sh
@@ -4,7 +4,7 @@ export OPENAI_API_KEY=
 MODEL_NAME="Mistral-7B-Base-SFT-SimPO"
 
 # 生成回答
-python gen_api_answer.py --model $MODEL_NAME --openai-api-base $OPENAI_BASE_URL --parallel 16
+python gen_api_answer.py --model $MODEL_NAME --openai-api-base http://localhost:8000/v1 --parallel 16
 
 # 生成评估
 python gen_judgment.py --model-list $MODEL_NAME --parallel 16 --judge-model deepseek-chat
