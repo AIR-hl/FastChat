d8d03fabc949af877674d9aa83b1a5a12a8c1397
许世越 <1531845081@qq.com>
2025-01-16 16:07:40 +0800

    script

--- fastchat/llm_judge/run.sh
+++ fastchat/llm_judge/run.sh
@@ -1,11 +1,13 @@
 export OPENAI_BASE_URL=https://api.deepseek.com/v1
 export OPENAI_API_KEY=
 
+MODEL_NAME="Mistral-7B-Base-SFT-SimPO"
+
 # 生成回答
-python gen_api_answer.py --model [MODEL-NAME] --openai-api-base http://localhost:8000/v1 --parallel 26
+python gen_api_answer.py --model $MODEL_NAME --openai-api-base $OPENAI_BASE_URL --parallel 16
 
 # 生成评估
-python gen_judgment.py --model-list [MODEL-NAME] --parallel 16 --judge-model deepseek-chat
+python gen_judgment.py --model-list $MODEL_NAME --parallel 16 --judge-model deepseek-chat
 
 # 展示结果
-python show_result.py --model-list [MODEL-NAME] --judge deepseek-chat
+python show_result.py --model-list $MODEL_NAME --judge deepseek-chat
